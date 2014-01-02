df5c69aaebc28ba5134b823abdd0fcdbf8d09ceb
vb8188 <vb8188@vm-0.vb8188.koding.kd.io>
2014-01-02 20:25:44 +0000

    c++ files first commit

---
+++ halfofsqr.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int n=5; n>0; n--)
+    {
+        for (int j=n; j>0; j-- )
+        {
+            cout<<"#";
+        }
+        cout<<"\n";
+    }
+}
---
+++ sidetri.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int n=0; n<5; n++)
+    {
+        cout<<"#";
+        //for (int j=n; j<5; j++ )
+        //{
+        //    cout<<"#";
+        //}
+        cout<<"\n";
+    }
+}
