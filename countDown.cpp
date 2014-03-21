9e74ff6358bc82de558fa63440075dffb5c06f66
vb8188 <vb8188@vm-0.vb8188.koding.kd.io>
2014-03-20 20:00:04 -0700

    3/20/2014

---
+++ countDown.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+/**
+int main()
+{
+    for(int row=0;row <= 5;row++){
+        for (int col=row;col <=5; col++){
+            cout << "#";
+        }
+    cout <<"\n";
+    }
+}
+**/
+int main()
+{
+    for(int row=0;row <= 5;row++){
+        for (int col=row;col <=5; col++){
+            cout << "#";
+        }
+    cout <<"\n";
+    }
+}
